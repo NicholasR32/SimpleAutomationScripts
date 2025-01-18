6ab8506235706f0b83ab35eef9717b617140186c
NicholasR32 <98115878+NicholasR32@users.noreply.github.com>
2025-01-18 02:46:20 -0500

    Refactor autoexpedition

--- Palworld/README.md
+++ Palworld/README.md
@@ -1,13 +1,13 @@
 #### How to automate expeditions when AFK:
-1. Install stuff for the script to work
+1. **Install stuff for the script to work**
     1. Install [Python](https://www.python.org/downloads/). This also installs pip, Python's package manager.
     2. Open Command Prompt or any terminal.
     3. Run `pip install pyautogui`. This package allows the script to control your mouse and keyboard.
-2. Play Palworld (the fun part!)
+2. **Play Palworld (the fun part!)**
     1. Enter a world and go to your base.
     2. Choose an expedition, and see how long it takes when you Auto-Assign. Note this time for later. Then cancel the expedition.
     3. Stand in front of the expedition station so you can interact with it, but don't open the station's interface.
-3. Run the script
+3. **Run the script**
     1. Download `autoexpedition.py`.
     2. Assuming the file is in your Downloads folder, run `cd Downloads` to navigate to that folder, so the script can be accessed. (cd = change directory)
     3. Run the script with `python autoexpedition.py`.
@@ -17,3 +17,7 @@
         3. Whether an expedition is currently in progress, since you'll have to wait for that one to finish first.
     5. Answer these prompts in the terminal so the script can account for those situations.
     6. Done! It should be fine to leave your computer, and the script will automatically claim and deploy new expeditions when the timer finishes. It is safe to use your mouse and keyboard while it is running, just ensure that Palworld is focused again when the timer finishes so the clicks reach the game properly. Otherwise, you'll have to rerun the script.
+4. **Making sure it works**
+    1. Before AFKing on a long 1-hour expedition, I highly recommend testing it with the first expedition, which can take as low as 5 minutes with level 30+ pals. Repeat the steps above with the first expedition, making sure to input how long it will take when you auto-assign.
+    2. Once it finishes, verify that the script uses your mouse to claim the rewards and then deploy another one.
+    3. If it works, you can AFK with a longer expedition. If it doesn't, please reach out to me with any issues.
--- Palworld/autoexpedition.py
+++ Palworld/autoexpedition.py
@@ -3,6 +3,54 @@
 import pyautogui as pag
 import time
 
+# Convert a time in format MM:SS to seconds.
+def to_seconds(duration):
+    clock = duration.split(':')
+    return int(clock[0]) * 60 + int(clock[1])
+
+def claim_expedition(claim_button_pos):
+    # Open station
+    print('Pressing F to open expedition station')
+    pag.press('f')
+    pag.press('f')
+    time.sleep(1)
+    print('Pressing button to collect rewards')
+    pag.moveTo(*claim_button_pos,1)
+    pag.click()
+    time.sleep(1)
+    print('Exiting expedition window')
+    pag.press('esc')
+    time.sleep(1)
+
+def deploy_expedition(exp_button_pos, auto_assign_button_pos, start_button_pos):
+    print('Pressing F to open expedition station')
+    pag.press('f') # Palworld eats this first input for some reason
+    pag.press('f')
+    time.sleep(3)
+    print('Selecting chosen expedition')
+    pag.moveTo(*exp_button_pos,1)
+    pag.click()
+    time.sleep(1)
+    print('Pressing Auto-Assign button')
+    pag.moveTo(*auto_assign_button_pos, 1)
+    pag.click()
+    time.sleep(3)
+    print('Pressing Start button')
+    pag.moveTo(*start_button_pos, 1)
+    pag.click()
+    time.sleep(1)
+
+def wait_expedition(time_to_wait):
+    print(f'Waiting {time_to_wait // 60} minute(s) {time_to_wait % 60} seconds')
+    time.sleep(time_to_wait)
+    # while time_to_wait > 0:
+    #     wait = min(time_to_wait, 600) # Print status every 10 minutesff
+    #     print(f'{time_to_wait // 60}:{time_to_wait % 60} left')
+    #     time.sleep(wait)
+    #     time_to_wait -= wait
+    print(f'Waiting an extra 10 seconds...')
+    time.sleep(10)
+
 
 if __name__ == '__main__':
     # Get your screen's resolution.
@@ -13,46 +61,38 @@ if __name__ == '__main__':
     # The position of each button as a percentage of your screen's X and Y.
     auto_assign_button_pos = (0.275*X,0.83*Y)
     start_button_pos = (0.5*X,0.83*Y)
-    collect_button_pos = (0.8*X,0.197*Y)
+    claim_button_pos = (0.8*X,0.197*Y)
 
     # Positions of each expedition destination button.
     # Conveniently, each button takes about 10% of the screen's height.
     exp_buttons = [
-                  [0.37*X,0.3*Y], # Cave in the Grassland
-                  [0.37*X,0.4*Y], # Forest's Secret Realm
-                  [0.37*X,0.5*Y], # Volcanic Inferno Cave
-                  [0.37*X,0.6*Y], # Hidden Ruins of the Desert
-                  [0.37*X,0.7*Y], # Frozen Cave of the Snow Mountain
-                  [0.37*X,0.8*Y]  # Spirit Blossom Cave of Sakurajima
-                ]
+                  [0.35*X,0.3*Y], # Cave in the Grassland
+                  [0.35*X,0.4*Y], # Forest's Secret Realm
+                  [0.35*X,0.5*Y], # Volcanic Inferno Cave
+                  [0.35*X,0.6*Y], # Hidden Ruins of the Desert
+                  [0.35*X,0.7*Y], # Frozen Cave of the Snow Mountain
+                  [0.35*X,0.8*Y]  # Spirit Blossom Cave of Sakurajima
+                  ]
     # TODO: add scrolling so 7th exp can be selected
     # NOTICE: Upon starting, this script assumes that a destination hasn't been chosen yet.
     #         If a destination already says 'Enrolled' on it, the station will go straight to
     #         the pal assigning window when opened. The extra click shouldn't matter.
     expeditions_completed = 0
-    print('Script started')
     exp_id = int(input('Which expedition are you running? 1 = Cave in the Grassland, 2 = Forest\'s Secret Realm, etc.\n'))
-    exp_time = int(input('Click Auto-Assign. How long will it take, in minutes?\n'))
+    exp_time_raw = input('Click Auto-Assign. How long will it take? Enter in format MM:SS. (Example: 48:30 = 48 minutes 30 seconds.)\n')
+    exp_time = to_seconds(exp_time_raw)
+
     in_progress = input('Is an expedition already in progress? [Y/N]\n')
     if in_progress in ['Y','y']:
-        time_to_wait = int(input('How much longer, in minutes? Round up to nearest minute.\n'))
+        duration_raw = input('How much longer, in minutes? Enter in format MM:SS.\n')
+        duration = to_seconds(duration_raw)
+
         print('Script activated! Please make sure Palworld is focused when the timer ends.')
-        time.sleep(time_to_wait*60+10)
+        wait_expedition(duration)
+
         print('Expedition done!')
         expeditions_completed += 1
-        # Reopen station
-        print('Pressing F to open expedition station')
-        pag.press('f')
-        pag.press('f')
-        time.sleep(1)
-        print('Pressing button to collect rewards')
-        pag.moveTo(*collect_button_pos,1)
-        pag.click()
-        time.sleep(1)
-        print('Exiting expedition window')
-        pag.press('esc')
-        time.sleep(1)
-        expeditions_completed+=1
+        claim_expedition(claim_button_pos)
         print(f'Expeditions completed: {expeditions_completed}')
         print('Now entering full AFK expedition loop.')
 
@@ -60,46 +100,18 @@ if __name__ == '__main__':
         print('Please refocus on Palworld.')
         print('Waiting 3 seconds for window focus...')
         time.sleep(3)
-        print('Pressing F to open expedition station')
-        pag.press('f') # Palworld eats this first input for some reason
-        pag.press('f')
-        time.sleep(3)
-        print('Selecting chosen expedition')
-        pag.moveTo(*exp_buttons[exp_id-1],1)
-        pag.click()
-        time.sleep(1)
-        print('Pressing Auto-Assign button')
-        pag.moveTo(*auto_assign_button_pos, 1)
-        pag.click()
-        time.sleep(3)
-        print('Pressing Start button')
-        pag.moveTo(*start_button_pos, 1)
-        pag.click()
-        time.sleep(1)
+        deploy_expedition(exp_buttons[exp_id-1],auto_assign_button_pos,start_button_pos)
         print('Expedition started! It is safe to use your mouse and keyboard while it finishes.\n')
-        if expeditions_completed < 2:
+
+        if expeditions_completed < 1:
             print('NOTE: Please make sure the right expedition was selected. You may have to manually enroll in the correct one beforehand.\n')
-        # By default, the time for each expedition is 30 min for expeditions 1-2, 45 min for 3-4, and 60 min for 5-7.
-        # Change this depending on your pals' total firepower, which can reduce the time required.
-        # Example: The first expedition can take as low as 5 minutes w/ high-level pals, which is useful for testing.
+
         print('Waiting for expedition to finish...')
         print('Please ensure Palworld is focused and you are in the correct position when the expedition timer ends.\n')
-        time.sleep(exp_time*60+10)
-        # It is safe to use your computer during this waiting phase,
-        # just ensure Palworld is focused and the expedition station is in range when the timer ends.
-        print('Expedition done!')
+        wait_expedition(exp_time)
 
-        # Reopen station
-        print('Pressing F to open expedition station')
-        pag.press('f')
-        pag.press('f')
-        time.sleep(1)
-        print('Pressing button to collect rewards')
-        pag.moveTo(*collect_button_pos,1)
-        pag.click()
-        time.sleep(1)
-        print('Exiting expedition window')
-        pag.press('esc')
-        time.sleep(1)
+        print('Expedition done!')
+        claim_expedition(claim_button_pos)
         expeditions_completed+=1
         print(f'Expeditions completed: {expeditions_completed}\n')
+
